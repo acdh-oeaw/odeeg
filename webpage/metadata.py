9d23ad76d2cc01f401258ddc89a417c9cf26c6f7
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-01-07 12:59:39 +0100

    info props added

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -3,7 +3,7 @@
 
 PROJECT_METADATA = {
     'title': 'ODEEG',
-    'author': 'Peter Andorfer',
+    'author': 'Peter Andorfer, Martina Trognitz',
     'subtitle': 'Online Database for Research on Antique Mediterranean Pottery',
     'description': """The ODEEG project has been funded by the go!digital programme 2016, Austria, and is connected to the wider CVA (Corpus Vasorum Antiquorum) project. Initially, the objectives of ODEEG were to: (1) Digitally document ancient Greek and Cypriot vases in 3D; (2) Ensure long-term digital preservation of the documentation; (3) Make a variety of data (e.g., 3D models, photographs, scientific illustrations, and text) available for researchers via a dedicated online database, referencing to the Beazley Archive (University of Oxford). By providing a publicly available, long-term, online archive, we aimed at creating an ideal foundation to gain further knowledge and help answering innovative scientific questions dealing with interior and exterior measurements of ancient vessels.""",
     'github': 'https://github.com/acdh-oeaw/odeeg',
@@ -11,5 +11,10 @@ PROJECT_METADATA = {
     'purpose_en': 'The purpose of ODEEG is the publication of research data.',
     'version': '0.0.1',
     'matomo_id': '92',
-    'matomo_url': '//piwik.apollo.arz.oeaw.ac.at/'
+    'matomo_url': '//piwik.apollo.arz.oeaw.ac.at/',
+    'social_media': [
+        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
+        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
+    ],
+    'app_type': 'database',
 }
