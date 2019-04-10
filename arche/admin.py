5827644cc162a9c594b3ce4e852efd099bef2dbc
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-04-10 12:48:39 +0200

    tabula rasa

--- arche/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Collection, Project, Resource
-
-admin.site.register(Collection)
-admin.site.register(Project)
-admin.site.register(Resource)
--- dobjects/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DobjectsConfig(AppConfig):
-    name = 'dobjects'
