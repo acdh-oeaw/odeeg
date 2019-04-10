2a5939b867fe5f4bc6ef350da76e2f7988064cc0
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-04-10 13:10:39 +0200

    imprint generated via imprint-service

---
+++ README.md
@@ -0,0 +1,15 @@
+# README
+
+## about
+
+The ODEEG project has been funded by the go!digital programme 2016, Austria, and is connected to the wider CVA (Corpus Vasorum Antiquorum) project. Initially, the objectives of ODEEG were to: (1) Digitally document ancient Greek and Cypriot vases in 3D; (2) Ensure long-term digital preservation of the documentation; (3) Make a variety of data (e.g., 3D models, photographs, scientific illustrations, and text) available for researchers via a dedicated online database, referencing to the Beazley Archive (University of Oxford). By providing a publicly available, long-term, online archive, we aimed at creating an ideal foundation to gain further knowledge and help answering innovative scientific questions dealing with interior and exterior measurements of ancient vessels.
+
+## install
+
+1. clone the repo
+2. create a virtual env in the root of the repo (you can put the created folder on the ignore list)
+3. activate the virtual env
+4. install dependencies (`pip install -r requirements.txt`)
+5. adapt settings file and folder to your needs (e.g. add server.py with according settings)
+6. makemigrations and migrate `python manage.py makemigrations|migrate --settings=odeeg.settings.dev`
+7. spin up the (dev) server: `python manage.py runserver --settings=odeeg.settings.dev`
---
+++ webpage/metadata.py
@@ -0,0 +1,21 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'ODEEG',
+    'author': 'Peter Andorfer, Martina Trognitz',
+    'subtitle': 'Online Database for Research on Antique Mediterranean Pottery',
+    'description': """The ODEEG project has been funded by the go!digital programme 2016, Austria, and is connected to the wider CVA (Corpus Vasorum Antiquorum) project. Initially, the objectives of ODEEG were to: (1) Digitally document ancient Greek and Cypriot vases in 3D; (2) Ensure long-term digital preservation of the documentation; (3) Make a variety of data (e.g., 3D models, photographs, scientific illustrations, and text) available for researchers via a dedicated online database, referencing to the Beazley Archive (University of Oxford). By providing a publicly available, long-term, online archive, we aimed at creating an ideal foundation to gain further knowledge and help answering innovative scientific questions dealing with interior and exterior measurements of ancient vessels.""",
+    'github': 'https://github.com/acdh-oeaw/odeeg',
+    'purpose_de': 'Ziel von ODEEG ist die Publikation von Forschungsdaten.',
+    'purpose_en': 'The purpose of ODEEG is the publication of research data.',
+    'version': '0.0.1',
+    'matomo_id': '92',
+    'matomo_url': '//piwik.apollo.arz.oeaw.ac.at/',
+    'social_media': [
+        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
+        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
+    ],
+    'app_type': 'database',
+    'imprint': '/imprint'
+}
