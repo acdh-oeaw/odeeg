6ab46e9df4a98f92618b902e3c7777c68c4cc99c
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2019-07-08 17:15:18 +0200

    changes to adjust layout to desired new version of fundament with changes in base.html new static files (images)

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -4,8 +4,11 @@
 PROJECT_METADATA = {
     'title': 'ODEEG',
     'author': 'Peter Andorfer, Martina Trognitz',
-    'subtitle': 'Online Database for Research on Antique Mediterranean Pottery',
-    'description': """The ODEEG project has been funded by the go!digital programme 2016, Austria, and is connected to the wider CVA (Corpus Vasorum Antiquorum) project. Initially, the objectives of ODEEG were to: (1) Digitally document ancient Greek and Cypriot vases in 3D; (2) Ensure long-term digital preservation of the documentation; (3) Make a variety of data (e.g., 3D models, photographs, scientific illustrations, and text) available for researchers via a dedicated online database, referencing to the Beazley Archive (University of Oxford). By providing a publicly available, long-term, online archive, we aimed at creating an ideal foundation to gain further knowledge and help answering innovative scientific questions dealing with interior and exterior measurements of ancient vessels.""",
+    'subtitle_en': 'Online database for research on the development of vessel shapes and dimensions',
+    'subtitle_de': 'Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen',
+    'description': """ODEEG (Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen) is an online database for research on the development of ancient pottery shapes and dimensions, with a special focus on 3D models and filling capacities, and is connected to the wider CVA project.
+Presently, ODEEG includes fine-wear painted vessels, from ancient Greek territories and Cyprus. Whereas the period encompasses 10th–4th century BCE, approximately from the Geometric period to the Hellenistic period.
+""",
     'github': 'https://github.com/acdh-oeaw/odeeg',
     'purpose_de': 'Ziel von ODEEG ist die Publikation von Forschungsdaten.',
     'purpose_en': 'The purpose of ODEEG is the publication of research data.',
