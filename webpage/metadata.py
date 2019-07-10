d18233fa8896068cba9bdbc7abfcaafee4ecd90e
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2019-07-10 16:07:48 +0200

    Base template done. Index page mostly done. About page almost done (image missing).

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -3,10 +3,10 @@
 
 PROJECT_METADATA = {
     'title': 'ODEEG',
-    'author': 'Peter Andorfer, Martina Trognitz',
+    'author': 'Publisher: Institute for the Study of Ancient Culture (IKAnt), ÖAW: Claudia Lang-Auinger, Vera Moitinho de Almeida, Stefan Spelitz.\n Technical Implementation & Design: Austrian Centre for Digital Humanities (ACDH), ÖAW: Martina Trognitz, Peter Andorfer and Institute for the Study of Ancient Culture (IKAnt), ÖAW: Vera Moitinho de Almeida.',
     'subtitle_en': 'Online database for research on the development of vessel shapes and dimensions',
     'subtitle_de': 'Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen',
-    'description': """ODEEG (Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen) is an online database for research on the development of ancient pottery shapes and dimensions, with a special focus on 3D models and filling capacities, and is connected to the wider CVA project.
+    'description': """ODEEG (Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen) is an online database for research on the development of ancient pottery shapes and dimensions, with a special focus on 3D models and filling capacities, and is connected to the wider <a href="https://www.oeaw.ac.at/en/ancient/publications/overview/publikationen-monant/corpus-vasorum-antiquorum/">CVA project</a>.
 Presently, ODEEG includes fine-wear painted vessels, from ancient Greek territories and Cyprus. Whereas the period encompasses 10th–4th century BCE, approximately from the Geometric period to the Hellenistic period.
 """,
     'github': 'https://github.com/acdh-oeaw/odeeg',
@@ -16,9 +16,11 @@ Presently, ODEEG includes fine-wear painted vessels, from ancient Greek territor
     'matomo_id': '92',
     'matomo_url': '//piwik.apollo.arz.oeaw.ac.at/',
     'social_media': [
-        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
-        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
     ],
     'app_type': 'database',
     'imprint': '/imprint'
 }
+
+# 'social_media': [
+#        ('data-feather twitter', 'https://twitter.com/ACDH_OeAW'),
+#        ('data-feather youtube', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
