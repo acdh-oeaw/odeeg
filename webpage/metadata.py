58a01b94259c4ba78e0f70a0ce5fa9ecbd3838cf
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2019-07-26 12:32:53 +0200

    More search fields from class Object for search indexing.

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -1,3 +1,4 @@
+from django.utils.safestring import mark_safe
 # this files contains basic metadata about the project. This data will be used
 # (by default) in the base.html and index.html
 
@@ -6,9 +7,9 @@ PROJECT_METADATA = {
     'author': 'Publisher: Institute for the Study of Ancient Culture (IKAnt), ÖAW: Claudia Lang-Auinger, Vera Moitinho de Almeida, Stefan Spelitz.\n Technical Implementation & Design: Austrian Centre for Digital Humanities (ACDH), ÖAW: Martina Trognitz, Peter Andorfer and Institute for the Study of Ancient Culture (IKAnt), ÖAW: Vera Moitinho de Almeida.',
     'subtitle_en': 'Online database for research on the development of vessel shapes and dimensions',
     'subtitle_de': 'Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen',
-    'description': """ODEEG (Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen) is an online database for research on the development of ancient pottery shapes and dimensions, with a special focus on 3D models and filling capacities, and is connected to the wider <a href="https://www.oeaw.ac.at/en/ancient/publications/overview/publikationen-monant/corpus-vasorum-antiquorum/">CVA project</a>.
+    'description': mark_safe("""ODEEG (Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen) is an online database for research on the development of ancient pottery shapes and dimensions, with a special focus on 3D models and filling capacities, and is connected to the wider <a href="https://www.oeaw.ac.at/en/ancient/publications/overview/publikationen-monant/corpus-vasorum-antiquorum/">CVA project</a>.</br>
 Presently, ODEEG includes fine-wear painted vessels, from ancient Greek territories and Cyprus. Whereas the period encompasses 10th–4th century BCE, approximately from the Geometric period to the Hellenistic period.
-""",
+"""),
     'github': 'https://github.com/acdh-oeaw/odeeg',
     'purpose_de': 'Ziel von ODEEG ist die Publikation von Forschungsdaten.',
     'purpose_en': 'The purpose of ODEEG is the publication of research data.',
