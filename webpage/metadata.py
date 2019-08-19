13f4ca43eaeb2a4922b3f33bdd99bf89b301450e
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2019-08-19 13:53:50 +0200

    Amendments to base, index and about after feedback.

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -5,9 +5,9 @@ from django.utils.safestring import mark_safe
 PROJECT_METADATA = {
     'title': 'ODEEG',
     'author': 'Publisher: Institute for the Study of Ancient Culture (IKAnt), ÖAW: Claudia Lang-Auinger, Vera Moitinho de Almeida, Stefan Spelitz.\n Technical Implementation & Design: Austrian Centre for Digital Humanities (ACDH), ÖAW: Martina Trognitz, Peter Andorfer and Institute for the Study of Ancient Culture (IKAnt), ÖAW: Vera Moitinho de Almeida.',
-    'subtitle_en': 'Online database for research on the development of vessel shapes and dimensions',
+    'subtitle_en': 'Online database for research on the development of vessel shapes and capacities',
     'subtitle_de': 'Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen',
-    'description': mark_safe("""ODEEG (Online-Datenbank zur Erforschung der Entwicklung von Gefäßformen und -maßen) is an online database for research on the development of ancient pottery shapes and dimensions, with a special focus on 3D models and filling capacities, and is connected to the wider <a href="https://www.oeaw.ac.at/en/ancient/publications/overview/publikationen-monant/corpus-vasorum-antiquorum/">CVA project</a>.</br>
+    'description': mark_safe("""ODEEG is an open-access online database for research on the development of ancient pottery shapes and capacities. ODEEG is specially focused on 3D models, filling capacities, metadata, and long term archiving of the data, while applying LOD and FAIR principles. It is connected to the wider <a href="https://www.oeaw.ac.at/en/ancient/publications/overview/publikationen-monant/corpus-vasorum-antiquorum/">CVA project</a>.</br>
 Presently, ODEEG includes fine-wear painted vessels, from ancient Greek territories and Cyprus. Whereas the period encompasses 10th–4th century BCE, approximately from the Geometric period to the Hellenistic period.
 """),
     'github': 'https://github.com/acdh-oeaw/odeeg',
