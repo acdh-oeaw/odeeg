3570fcb818311d1f637aebf13c349965d6377a07
csae8092 <peter.andorfer@oeaw.ac.at>
2021-02-10 13:52:28 +0100

    wrote (failing) test

--- vases/tests.py
+++ vases/tests.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+from vases.models import Object
+from vases.arche_utils import create_query_sting
+
+QUERY_DICT = {
+    "key": "value",
+    "key1": "123"
+}
+
+
+class ArcheTest(TestCase):
+
+    def setUp(self):
+        self.item = Object.objects.all()[33]
+
+    def test_001_get_tiffs(self):
+        query_str = create_query_sting(QUERY_DICT)
+        self.assertIsInstance(query_str, 'str')
+
