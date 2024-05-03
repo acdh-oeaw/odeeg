9853470f39dd711e6832f05b544897289e67284f
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-03 13:51:42 +0200

    minor #15

--- start-server.sh
+++ start-server.sh
@@ -10,5 +10,6 @@ fi
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && python manage.py createsuperuser --no-input --noinput --email 'blank@email.com')
 fi
+python manage.py createcachetable
 python manage.py rebuild_index --noinput
 gunicorn odeeg.wsgi --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
