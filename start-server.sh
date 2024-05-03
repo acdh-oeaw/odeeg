6cde503417490b90ae3c0415887a7524da68ee41
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-03 13:35:03 +0200

    works locally and dockerized

--- start-server.sh
+++ start-server.sh
@@ -10,4 +10,5 @@ fi
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && python manage.py createsuperuser --no-input --noinput --email 'blank@email.com')
 fi
-gunicorn odeeg.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
+python manage.py rebuild_index --noinput
+gunicorn odeeg.wsgi --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
